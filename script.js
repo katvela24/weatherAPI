4e8ebe8c545a49d367848a11250fd94b44884c1d
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-03-05 15:45:19 -0500

    completed the history buttons

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 // fetch ("api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=21a340cdde7edefb7027c127cf3273cf");
 var city = $("#city");
-
-    
+var historyid = $("#history");
 var searchButton = $("#search-button");
+var cities = JSON.parse(localStorage.getItem("cities")) || []
 
 searchButton.click(function(){
     var cityValue = city.val();
     console.log("cityValue",cityValue);
     city.val("")
-
-    renderHistory(cityValue)    
+    renderHistory(cityValue,true)    
 
 })
-function renderHistory(cityValue){
-    console.log("RenderHistory",cityValue)
+function renderHistory(cityValue,clicked){
+    historyid.empty()
+    if(clicked){
+    
+        console.log("RenderHistory",cityValue)
+        let newCities = JSON.stringify(cities)
+        cities.push(cityValue)
+        console.log("cities",cities)
+        localStorage.setItem("cities",JSON.stringify(cities))
 
+    }
+    
+    // print corresponding button for each city we search for
+    $.each(cities,function(i,city){
+        console.log("checkcity",city)
+        let cityButton = $('<button class="city">')
+        cityButton.text(city)
+        historyid.append(cityButton)
+    })
 }
 // globalvariables
 // document.querySelector target the values of HTML objects
@@ -47,3 +62,5 @@ fetch ("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=21a34
     })
     // functionName(data)
     // google: how to target the DOM; how to get the DOMs value; how to append; for loops
+    
+    renderHistory("",false)

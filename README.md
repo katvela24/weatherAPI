b43f5699153e7acde47c27277f2d45c9dc4965e3
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-03-06 20:55:38 -0500

    pulled out five day forecast information

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# weatherAPI
+TITLE:
+
+Weather Dashboard (Server-Side APIs) for the Full-stack coding bootcamp at Columbia University
+
+DESCRIPTION:
+
+This weather dashboard allows a user to obain a city's current weather condition along with the five-day forecast. To access the data, third-party APIs were used by making requests with parameters to a URL. 
+
+ACCEPTANCE CRITERIA:
+
+Create a form input so that when the user searches for a city's weather conditions, that city is added to a search history after presenting the user with the current and future weather conditions.
+
+When viewing current weather conditions for that city, the city name, date, and an icon representations of the weather conditions (e.g. temperature, humidity and wind speed) are presented.
+
+The five-day forecast displays the date, an icon representation of weather conditions, the temperature, the wind speed and the humidity.
+
+When clicking on a city in the search history, then the user is presented with current and future conditions for that city.
+
+FEATURES
+
+OpenWeather API was used to retrieve the weather data
+Local storage was used to save search histories
+Previously searched cities get the current weather and five-day forecast
+jQuery event listeners help identify when a user searches for a city (including past cities) or when they clear the search history
+Bootsrap grid was used to determine rows and columns for all devices
+
+
+CREDITS:
+
+While this job was peformed with care by yours truly, 
+
+I cannot let the assistance of my classmates, AI, learning assistants, TAs, and professor go unnoticed.
+
+I look forward to your feedback. Be brutal.
+
+LICENSE:
+
+MIT License
+
+
+TESTS:
+
+None yet 
+
+LINK:
+
+ https://katvela24.github.io/passwordgenerator/
+
+SCREENSHOT:
+
+![ss](assets/Images/generate-password-screenshot.png) 
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,7 +6,7 @@ var cities = JSON.parse(localStorage.getItem("cities")) || []
 
 searchButton.click(function(){
     var cityValue = city.val();
-    console.log("cityValue",cityValue);
+    // console.log("cityValue",cityValue);
     city.val("")
     renderHistory(cityValue,true)    
 
@@ -17,36 +17,51 @@ function getCoord(city){
     fetch ("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=21a340cdde7edefb7027c127cf3273cf")
     .then(function(response){
         return response.json()
-        console.log (response[0].name)
+        // console.log (response[0].name)
     })
     .then(function(data){
-        console.log("city data",data)
-        console.log(data[0].name)
+        // console.log("city data",data)
+        // console.log(data[0].name)
         fetch ("http://api.openweathermap.org/data/2.5/forecast?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=imperial&appid=21a340cdde7edefb7027c127cf3273cf")
             .then(function(response){
                 return response.json()
 
             })
             .then(function(data){
-                console.log("city",data.city.name)
-                console.log("date",data.list[0].dt_txt)
-                console.log("icon",data.list[0].weather[0].icon)
-                console.log("temperature",data.list[0].main.temp)
-                console.log("windspeed",data.list[0].wind.speed)
-                console.log("humidity",data.list[0].main.humidity)
-                
-                currentData = {
-                    city:city,
-                    date:date,
-                    icon:icon,
-                    temperature:temperature,
-                    windspeed:windspeed,
-                    humidity:humidity,
+                let arr = []
+                // console.log("city",data.city.name)
+                for (const key in data.list) {
+                   
+                    if((Number(key)+1)%8 === 0){
+                        // console.log("key=",data.list[Number(key)]) 
+                        forecastData = {
+                            city:data.city.name,
+                            date:data.list[key].dt_txt,
+                            icon:data.list[key].weather[0].icon,
+                            temperature:data.list[key].main.temp,
+                            windspeed:data.list[key].wind.speed,
+                            humidity:data.list[key].main.humidity,
+                        }
+                      arr.push(forecastData)
+
+                    }
                 }
+                // console.log(arr)
+                return arr
+                // console.log("city",data.city.name)
+                // console.log("date",data.list[0].dt_txt)
+                // console.log("icon",data.list[0].weather[0].icon)
+                // console.log("temperature",data.list[0].main.temp)
+                // console.log("windspeed",data.list[0].wind.speed)
+                // console.log("humidity",data.list[0].main.humidity)
+                
              
             //    I need to call the function and pass the data
             // functionName(data)
             })
+            .then(function(forecast){
+                console.log(forecast)
+            }) 
             .catch(function(err){
                 console.log (err)
             })
@@ -61,17 +76,17 @@ function renderHistory(cityValue,clicked){
     historyid.empty()
     if(clicked){
     
-        console.log("RenderHistory",cityValue)
+        // console.log("RenderHistory",cityValue)
         let newCities = JSON.stringify(cities)
         cities.push(cityValue)
-        console.log("cities",cities)
+        // console.log("cities",cities)
         localStorage.setItem("cities",JSON.stringify(cities))
 
     }
     
     // print corresponding button for each city we search for
     $.each(cities,function(i,city){
-        console.log("checkcity",city)
+        // console.log("checkcity",city)
         let cityButton = $('<button class="city">')
         cityButton.text(city)
         historyid.append(cityButton)
